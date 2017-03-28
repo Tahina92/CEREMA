589411aa4b2317902971269a788a0162c0d347ae
Tahina92 <tahinajean92@gmail.com>
2017-03-28 15:00:27 +0000

    detection

--- capteur.py
+++ capteur.py
@@ -1,10 +1,15 @@
-#! /usr/bin/env python3
+#!/usr/bin/python3
 # coding: utf-8
+
+"""
+Autors: CEREMA/DTerSO/DALETT/ESAD-ZELT/ TAHINJANAHARY Jean Noël
+Projet: Capteur Bluetooth open Source et open Hardware
+"""
 
 from threading import Thread, Condition, Event
 import paho.mqtt.client as paho
-from random import *
 import sys, json, time 
+import bluetooth
 
 
 class Detection(Thread):
@@ -23,8 +28,10 @@ class Detection(Thread):
 		self.condition.acquire()
 		true = 0		
 		while true==0:
-			self.condition.wait(self.delais)
-			self.function("Horodate, MAC")
+			#self.condition.wait(self.delais)
+			nearby_devices = bluetooth.discover_devices(duration=self.delais, lookup_names=True, flush_cache= True, lookup_class=False)
+			print(nearby_devices)
+			self.function(nearby_devices)
 		self.condition.release()
 		
 	def get(self):
@@ -45,13 +52,12 @@ if __name__ == '__main__' :
 		print("Connected  with result code" + str(rc))
 		
 	def on_value (value):
-		"""Sending MAC horodate"""
+		"""Sending horodate MAC """
 		json_value = {}
 		json_value['value'] = str(value)
 		mqtts.publish("/publish/value", json.dumps(json_value))
-		print("envoyer")
-
-	detection = Detection(2,on_value)
+		
+	detection = Detection(1,on_value)
 	detection.start()
 	
 	mqtts = paho.Client()
