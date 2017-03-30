ec2b417cedfd7990e694ecbf718fd16cc30f5c2d
Tahina92 <tahinajean92@gmail.com>
2017-03-30 09:45:09 +0200

    Version projet

--- capteur.py
+++ capteur.py
@@ -9,39 +9,39 @@ Projet: Capteur Bluetooth open Source et open Hardware
 from threading import Thread, Condition, Event
 import paho.mqtt.client as paho
 import sys, json, time 
-import bluetooth
+import bluetooth, signal
 
 
 class Detection(Thread):
 	
-	def __init__(self, delais,fonction):
+	def __init__(self, delais,publish):
 		Thread.__init__(self)
 		print(delais)
-		self.delais = delais
-		self.function = fonction
+		self.__delais = delais
+		self.__publish = publish
 
-		self.condition = Condition()
-		self.executer = Event()
-		self.executer.set()
+		self.__condition = Condition()
+		self.__keep_running = Event()
+		self.__keep_running.set()
 		
 	def run(self):
-		self.condition.acquire()
-		true = 0		
-		while true==0:
-			#self.condition.wait(self.delais)
-			nearby_devices = bluetooth.discover_devices(duration=self.delais, lookup_names=True, flush_cache= True, lookup_class=False)
+		self.__condition.acquire()
+		self.__keep_running.isSet
+		while self.__keep_running.isSet:
+			nearby_devices = bluetooth.discover_devices(duration=self.__delais, lookup_names=True, flush_cache= True, lookup_class=False)
 			print(nearby_devices)
-			self.function(nearby_devices)
-		self.condition.release()
-		
+			self.__publish(nearby_devices)
+		self.__condition.release()
 	def get(self):
-		self.condition.acquire()
-		self.condition.notify()
-		self.condition.release()
-		
+		self.__condition.acquire()
+		self.__condition.notify()
+		self.__condition.release()		
 		
 	def stop(self):
-		self.executer.clear()
+		self.__keep_running.clear()
+		self.get()
+		print("Exit")
+	
 		
 		
 
@@ -51,15 +51,23 @@ if __name__ == '__main__' :
 	def on_connect(client, userdata, flags, rc):
 		print("Connected  with result code" + str(rc))
 		
-	def on_value (value):
+	def on_value (message):
 		"""Sending horodate MAC """
 		json_value = {}
-		json_value['value'] = str(value)
-		mqtts.publish("/publish/value", json.dumps(json_value))
+		json_value['Message'] = str(message)
+		mqtts.publish("/id_arc_OSM/value", json.dumps(json_value))
+	
+	def deconnection(signal, frame):
+		print("Exit")
+		mqtts.disconnect()
+		detection.stop()
+		print("Exit")
 		
 	detection = Detection(1,on_value)
 	detection.start()
+	signal.signal(signal.SIGINT, deconnection)  # Ctrl-c
 	
+	#Mise en connexion avec le serveur
 	mqtts = paho.Client()
 	mqtts.on_connect = on_connect
 	mqtts.connect("iot.eclipse.org", 1883, 60)
---
+++ start.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+# Autors: CEREMA/DTerSO/DALETT/ESAD-ZELT TAHINJANAHARY Jean Noël
+# Projet: Capteur Bluetooth open Source et open Hardware
+
+pkill python3
+pkill python
+
+declare -i x=0
+
+echo $i
+python3 capteur.py&
